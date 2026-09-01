7f512321b4cd492a2aa5a45bd7de58cc93683a7c
Ifham Fikri <ifhamfikri21@gmail.com>
2026-09-01 12:57:55 +0700

    Update database.js

--- blocks/database.js
+++ blocks/database.js
@@ -3,7 +3,7 @@ const mysql = require ('mysql2/promise');
 const db = mysql.createPool({
     host: process.env.DB_HOST || "localhost",
     user: process.env.DB_USER || "scalable",
-    password: process.env.DB_password,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_HOST || "scalable_one",
     waitForConnections: true,
     connectionLimit: 10
