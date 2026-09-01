41ae5e1245fdd0fe315d2523c3268b77445991b5
Ifham Fikri <ifhamfikri21@gmail.com>
2026-09-01 12:18:54 +0700

    Add files via upload

--- blocks/database.js
+++ blocks/database.js
@@ -1,10 +1,10 @@
 const mysql = require ('mysql2/promise');
 
 const db = mysql.createPool({
-    host: 'localhost',
-    user: 'mysql',
-    password: 'Ifhamfkr050706',
-    database: 'scalable_one',
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "mysql",
+    password: process.env.DB_password,
+    database: process.env.DB_HOST || "scalable_one",
     waitForConnections: true,
     connectionLimit: 10
 });
